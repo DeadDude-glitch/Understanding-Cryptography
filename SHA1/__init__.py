c1e37bc68d5b5b3645dcf9ad9037a548e2a9ada4
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-01-01 18:59:59 +0200

    enchanced class usability
    enchanced class representation

--- SHA1/__init__.py
+++ SHA1/__init__.py
@@ -11,6 +11,12 @@ class SHA1(object):
         self._message = b""
         self._cached_hash = None
 
+    def __str__(self):
+        self.hash()
+        return '{:08x}{:08x}{:08x}{:08x}{:08x}'.format(self._cached_hash[0],self._cached_hash[1],self._cached_hash[2],self._cached_hash[3],self._cached_hash[4])
+
+    def __repr__(self): return struct.pack('>5L', self.hash())
+
     @staticmethod
     def pad(padded_msg:bytes):
         # Pre-processing: Padding the message
@@ -24,7 +30,7 @@ class SHA1(object):
     @staticmethod
     def f(loop_number:int, h:list):
         if loop_number < 0 : raise ValueError("loop number must be a positive non-zero integer")
-        if len(h) != 3 : raise IndexError(f"h must be an array of 4 values, {len(h)} was given.")
+        if len(h) != 3 : raise IndexError(f"h must be an array of 4 values, but {len(h)} was given.")
         b, c ,d = h
         if   loop_number % 80 < 20: return (h[0] & h[1]) | ((~h[0]) & h[2])
         elif loop_number % 80 < 40: return  h[0] ^ h[1] ^ h[2]
@@ -34,10 +40,10 @@ class SHA1(object):
 
     @staticmethod
     def round(loop_number:int, h:list, word:int) -> list:
-        temp = ((h[0] << 5 | h[0] >> 27) & 0xFFFFFFFF) + SHA1.f(loop_number,h[1:4]) + h[4] + SHA1.k[int(loop_number/20)%4] + word & 0xFFFFFFFF
+        temp = ((h[0] << 5 | h[0] >> 27)) + SHA1.f(loop_number,h[1:4]) + h[4] + SHA1.k[int(loop_number/20)%4] + word & 0xFFFFFFFF
         h[4] = h[3]
         h[3] = h[2]
-        h[2] = (h[1] << 30 | h[1] >> 2) & 0xFFFFFFFF
+        h[2] = (h[1] << 30 | h[1] >> 2)
         h[1] = h[0]
         h[0] = temp
         return h
@@ -47,7 +53,7 @@ class SHA1(object):
         self._cached_hash = None
         return True
 
-    def hash(self, rounds:int=80, format='bytes'):
+    def hash(self, rounds:int=80) -> list:
         if self._cached_hash != None :
             h0, h1, h2, h3, h4 = self._cached_hash
         else:
@@ -66,6 +72,7 @@ class SHA1(object):
                 h0, h1, h2, h3, h4 = self.h
                 for j in range(rounds):
                     h0, h1, h2, h3, h4 = self.round(j,[h0,h1,h2,h3,h4],words[j])
+                # final addition
                 h0 = (self.h[0] + h0) & 0xFFFFFFFF
                 h1 = (self.h[1] + h1) & 0xFFFFFFFF
                 h2 = (self.h[2] + h2) & 0xFFFFFFFF
@@ -73,13 +80,7 @@ class SHA1(object):
                 h4 = (self.h[4] + h4) & 0xFFFFFFFF
             # Remember the current hash
             self._cached_hash = [h0, h1, h2, h3, h4]
-
-        # Produce the final hash
-        if format == 'hex':
-            return '{:08x}{:08x}{:08x}{:08x}{:08x}'.format(h0, h1, h2, h3, h4)
-        if format == "bytes":
-            return struct.pack('>5L', h0, h1, h2, h3, h4)
-        else: raise ValueError(f"format {format} is not supported")
+        return self._cached_hash
 
 # Example usage:
 if __name__ == '__main__':
@@ -90,8 +91,8 @@ if __name__ == '__main__':
         if len(argv) < 2: raise IndexError("No strings passed as arguments")
         for string in argv[1:]:
             _sha1.load(string)
-            print(f"(+) Data:\"{string}\"|Hash:{_sha1.hash(format='hex')}|Valid:{sha1(string.encode()).hexdigest()==_sha1.hash(format='hex')}")
+            print(f"(+) Data:\"{string}\"|Hash:{_sha1}|Valid:{sha1(string.encode()).hexdigest()==str(_sha1)}")
     except IndexError:
         string = input("(*) Enter Plain Text >. ")
         _sha1.load(string)
-        print(f"(+) Data:\"{string}\"|Hash:{_sha1.hash(format='hex')}|Valid:{sha1(string.encode()).hexdigest()==_sha1.hash(format='hex')}")Abstracted rounds into a static function
+        print(f"(+) Data:\"{string}\"|Hash:{_sha1}|Valid:{sha1(string.encode()).hexdigest()==str(_sha1)}")
